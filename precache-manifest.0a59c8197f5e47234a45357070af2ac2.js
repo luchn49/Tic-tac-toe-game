f849d76c5b062a0077f860abb4965f7a10e6fe0f
luchn49 <luchn49@gmail.com>
2020-01-08 02:46:19 +0700

    Updates

---
+++ precache-manifest.0a59c8197f5e47234a45357070af2ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b465f7fc30f238dd2e7d2e93ffaab33",
+    "url": "/Tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "2abdf5141ce735674c48",
+    "url": "/Tic-tac-toe-game/static/css/main.ebc7fee9.chunk.css"
+  },
+  {
+    "revision": "8dd64162c77b38b13013",
+    "url": "/Tic-tac-toe-game/static/js/2.1365b90e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tic-tac-toe-game/static/js/2.1365b90e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2abdf5141ce735674c48",
+    "url": "/Tic-tac-toe-game/static/js/main.da651a3f.chunk.js"
+  },
+  {
+    "revision": "def14250a96e9dd97060",
+    "url": "/Tic-tac-toe-game/static/js/runtime-main.8aabc430.js"
+  }
+]);
